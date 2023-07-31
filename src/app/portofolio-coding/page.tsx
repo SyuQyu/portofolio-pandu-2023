b57254cd7b6dc67891f3efd25646a426085c0c9d
Pandu <104115882+SyuQyu@users.noreply.github.com>
2023-07-31 11:55:14 +0700

    add portofolio page

--- src/app/portofolio-coding/page.tsx
+++ src/app/portofolio-coding/page.tsx
@@ -8,34 +8,54 @@ import {
     Typography,
     Button,
 } from "@material-tailwind/react";
+import ImageWithFallback from '@/components/common/imageWithFallback';
 import { Coding } from '@/contants/dataCoding';
+import Link from 'next/link';
+import { MdPreview } from "react-icons/md";
+import { motion } from 'framer-motion';
 export default function PortofolioCoding() {
     return (
         <Container>
-            <div className="w-full h-screen flex flex-col justify-between">
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-12">
                 {
                     Coding?.map((data, index) =>
-                        <Card className="mt-6 w-96" key={index}>
-                            <CardHeader color="blue-gray" className="relative h-56">
-                                <img
-                                    src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-                                    alt="card-image"
-                                />
-                            </CardHeader>
-                            <CardBody>
-                                <Typography variant="h5" color="blue-gray" className="mb-2">
-                                    {data.projectName}
-                                </Typography>
-                                <Typography>
-                                    {data.desc}
-                                </Typography>
-                            </CardBody>
-                            {/* <CardFooter className="pt-0">
-                        <Button>Github</Button>
-                    </CardFooter> */}
-                        </Card>
+                        <motion.div whileHover={{ scale: 1.1 }} className="h-full">
+                            <Card className="mt-6 w-full h-full bg-gray-800/50 flex flex-col justify-between" key={index}>
+                                <CardHeader className="relative h-56 shadow-lg shadow-anzac-200/30">
+                                    <ImageWithFallback
+                                        priority={true}
+                                        width={0}
+                                        height={0}
+                                        sizes='100vw'
+                                        className='w-full h-full max-w-full rounded-lg object-cover' src={data.image} alt="" />
+                                </CardHeader>
+                                <CardBody>
+                                    <Typography variant="h5" color="white" className="mb-2">
+                                        {data.projectName}
+                                    </Typography>
+                                    <Typography color="white">
+                                        {data.desc}
+                                    </Typography>
+                                </CardBody>
+                                <CardFooter className="pt-0 flex flex-row justify-end items-center space-x-4">
+                                    <Link href={data.projectLink.github}>
+                                        <ImageWithFallback
+                                            priority={true}
+                                            src={'/image/sosmed/github (1).svg'}
+                                            alt={`Image for github`}
+                                            width={0}
+                                            height={0}
+                                            sizes='100vw'
+                                            className='w-[32px] sm:h-[32px] object-contain'
+                                        />
+                                    </Link>
+                                    <Link href={data.projectLink.github}>
+                                        <MdPreview className='text-[32px] text-white' />
+                                    </Link>
+                                </CardFooter>
+                            </Card>
+                        </motion.div>
                     )
-
                 }
             </div>
         </Container>
