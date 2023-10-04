ed1e4f96ca268fef658e8ced68ed1c0b057359f4
Pandu <104115882+SyuQyu@users.noreply.github.com>
2023-10-04 10:42:01 +0700

    update google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PD7JJF8Z');
+      `}
+      </Script>
       <body className={clsx('scroll-smooth', POPPINS.variable, ROBOTO_MONO.variable)}>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PD7JJF8Z"
+          height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
         <DefaultLayout>{children}</DefaultLayout>
         <Script src="https://www.googletagmanager.com/gtag/js?id=G-HVM4ZSN3ZG" />
         <Script id="google-analytics">
