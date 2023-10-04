c0b33d8acdf4df2e5b0a6325b5c7aebef8e2abca
Pandu <104115882+SyuQyu@users.noreply.github.com>
2023-10-04 10:14:29 +0700

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import Script from 'next/script'
 import '@/styles/index.scss';
 import { POPPINS, ROBOTO_MONO } from '@/contants/fonts';
 
@@ -17,6 +18,16 @@ export default function RootLayout({
     <html lang="en">
       <body className={clsx('scroll-smooth', POPPINS.variable, ROBOTO_MONO.variable)}>
         <DefaultLayout>{children}</DefaultLayout>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-HVM4ZSN3ZG" />
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-HVM4ZSN3ZG');
+        `}
+        </Script>
       </body>
     </html>
   )
